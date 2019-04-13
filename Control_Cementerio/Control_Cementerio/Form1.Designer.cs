4d9a126d574e5fa11a06b52aacae168f7f87a53f
JeffersonMedina <medina_pein@hotmail.com>
2019-04-13 18:57:45 -0500

    Desarrolador: Dieseño Login

--- Control_Cementerio/Control_Cementerio/Form1.Designer.cs
+++ Control_Cementerio/Control_Cementerio/Form1.Designer.cs
@@ -28,20 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.lbl_Login = new System.Windows.Forms.Label();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.link_contraseña = new System.Windows.Forms.LinkLabel();
+            this.btn_minimizar = new System.Windows.Forms.PictureBox();
+            this.btn_cerrar = new System.Windows.Forms.PictureBox();
+            this.pct_municipio = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_minimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_cerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_municipio)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(218)))), ((int)(((byte)(156)))));
+            this.panel1.Controls.Add(this.pct_municipio);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 330);
+            this.panel1.TabIndex = 0;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(313, 73);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(302, 20);
+            this.txt_usuario.TabIndex = 2;
+            this.txt_usuario.Text = "Usuario";
+            this.txt_usuario.Enter += new System.EventHandler(this.txt_usuario_Enter);
+            this.txt_usuario.Leave += new System.EventHandler(this.txt_usuario_Leave);
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(313, 137);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(302, 20);
+            this.txt_password.TabIndex = 3;
+            this.txt_password.Text = "Contraseña";
+            this.txt_password.Enter += new System.EventHandler(this.txt_password_Enter);
+            this.txt_password.Leave += new System.EventHandler(this.txt_password_Leave);
+            // 
+            // lbl_Login
+            // 
+            this.lbl_Login.AutoSize = true;
+            this.lbl_Login.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Login.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lbl_Login.Location = new System.Drawing.Point(257, 13);
+            this.lbl_Login.Name = "lbl_Login";
+            this.lbl_Login.Size = new System.Drawing.Size(64, 20);
+            this.lbl_Login.TabIndex = 3;
+            this.lbl_Login.Text = "LOGIN";
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.DarkGray;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_login.ForeColor = System.Drawing.Color.White;
+            this.btn_login.Location = new System.Drawing.Point(383, 192);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(173, 30);
+            this.btn_login.TabIndex = 1;
+            this.btn_login.Text = "Acceder";
+            this.btn_login.UseVisualStyleBackColor = false;
+            // 
+            // link_contraseña
+            // 
+            this.link_contraseña.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.link_contraseña.AutoSize = true;
+            this.link_contraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link_contraseña.LinkColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.link_contraseña.Location = new System.Drawing.Point(313, 256);
+            this.link_contraseña.Name = "link_contraseña";
+            this.link_contraseña.Size = new System.Drawing.Size(120, 13);
+            this.link_contraseña.TabIndex = 4;
+            this.link_contraseña.TabStop = true;
+            this.link_contraseña.Text = "¿Olvidó su Contraseña?";
+            // 
+            // btn_minimizar
+            // 
+            this.btn_minimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.btn_minimizar.Image = global::Control_Cementerio.Properties.Resources.minimizar;
+            this.btn_minimizar.Location = new System.Drawing.Point(708, 2);
+            this.btn_minimizar.Name = "btn_minimizar";
+            this.btn_minimizar.Size = new System.Drawing.Size(32, 31);
+            this.btn_minimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_minimizar.TabIndex = 7;
+            this.btn_minimizar.TabStop = false;
+            this.btn_minimizar.Click += new System.EventHandler(this.btn_minimizar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.btn_cerrar.Image = ((System.Drawing.Image)(resources.GetObject("btn_cerrar.Image")));
+            this.btn_cerrar.Location = new System.Drawing.Point(746, 2);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(32, 31);
+            this.btn_cerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_cerrar.TabIndex = 6;
+            this.btn_cerrar.TabStop = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // pct_municipio
+            // 
+            this.pct_municipio.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pct_municipio.Image = ((System.Drawing.Image)(resources.GetObject("pct_municipio.Image")));
+            this.pct_municipio.Location = new System.Drawing.Point(0, 0);
+            this.pct_municipio.Name = "pct_municipio";
+            this.pct_municipio.Size = new System.Drawing.Size(250, 330);
+            this.pct_municipio.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pct_municipio.TabIndex = 0;
+            this.pct_municipio.TabStop = false;
+            this.pct_municipio.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pct_municipio_MouseDown);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(439, 425);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.ClientSize = new System.Drawing.Size(780, 330);
+            this.Controls.Add(this.btn_minimizar);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.link_contraseña);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.lbl_Login);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btn_minimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_cerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_municipio)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Label lbl_Login;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.LinkLabel link_contraseña;
+        private System.Windows.Forms.PictureBox pct_municipio;
+        private System.Windows.Forms.PictureBox btn_cerrar;
+        private System.Windows.Forms.PictureBox btn_minimizar;
     }
 }
 
--- Control_Cementerio/Control_Cementerio/Form1.cs
+++ Control_Cementerio/Control_Cementerio/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Control_Cementerio
 {
@@ -15,6 +16,68 @@ namespace Control_Cementerio
         public Form1()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnnd, int wmsg, int wparam, int lparam);
+        
+
+        private void txt_usuario_Enter(object sender, EventArgs e)
+        {
+            if (txt_usuario.Text == "Usuario")
+            {
+                txt_usuario.Text = "";
+            }
+        }
+
+        private void txt_usuario_Leave(object sender, EventArgs e)
+        {
+            if(txt_usuario.Text == "")
+            {
+                txt_usuario.Text = "Usuario";
+            }
+        }
+
+        private void txt_password_Enter(object sender, EventArgs e)
+        {
+            if(txt_password.Text == "Contraseña")
+            {
+                txt_password.Text = "";
+                txt_password.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txt_password_Leave(object sender, EventArgs e)
+        {
+            if(txt_password.Text == "")
+            {
+                txt_password.Text = "Contraseña";
+                txt_password.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+
+        private void btn_minimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void pct_municipio_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
